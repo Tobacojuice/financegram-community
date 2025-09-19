2aaeef2ae41220b490de77e52706b632267ac22d
Tobacojuice <ilopezmelendo@gmail.com>
2025-09-19 02:11:45 +0200

    chore: remove generated javascript duplicates

--- Desktop/WAR/testingbeta/financegram-community-master/src/context/session.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-export const SessionContext = createContext(null);
-export function useSessionContext() {
-    const ctx = useContext(SessionContext);
-    if (!ctx) {
-        throw new Error('useSessionContext must be used within a SessionContext provider');
-    }
-    return ctx;
-}
